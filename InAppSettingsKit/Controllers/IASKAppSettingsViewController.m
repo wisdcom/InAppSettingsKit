8da115c1449c2932a95821cdb8c1bc08e07d9a7a
Kenneth P. Wong <wpk@wisd.com>
2019-02-28 16:09:35 -0800

    UITableViewCellSeparatorStyleSingleLineEtched deprecated

--- InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
@@ -173,12 +173,14 @@ CGRect IASKCGRectSwap(CGRect rect);
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    if ([self isPad]) {
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
-        if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1)	// don't use etched style on iOS 7
-#endif
-            self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLineEtched;
-    }
+	
+	// WPK: UITableViewCellSeparatorStyleSingleLineEtched deprecated since iOS 11; don't use.
+//    if ([self isPad]) {
+//#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
+//        if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1)	// don't use etched style on iOS 7
+//#endif
+//            self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLineEtched;
+//    }
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapToEndEdit:)];   
     tapGesture.cancelsTouchesInView = NO;
     [self.tableView addGestureRecognizer:tapGesture];
